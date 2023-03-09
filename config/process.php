337d8f2fa43bbe410891b278e036f53f0c7e5eff
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-03-09 14:14:19 -0300

    add query for create contact in db, returning session msg

--- config/process.php
+++ config/process.php
@@ -8,8 +8,27 @@ include_once("url.php");
 $data = $_POST;
 // Modificações no Banco
 if (!empty($data)) {
-  print_r($data);
   if ($data["type"] === "create") {
+    $name = $data["name"];
+    $phone = $data["phone"];
+    $observations = $data["observations"];
+
+    $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":phone", $phone);
+    $stmt->bindParam(":observations", $observations);
+
+    try {
+      $stmt->execute();
+      $_SESSION["msg"] = "Contato criado com sucesso";
+    } catch (PDOException $e) {
+      // Erro na conexão
+      $error = $e->getMessage();
+      echo "Erro: $error";
+    }
+    // Redirecionando o usuario para a home
+    header("Location:" . $BASE_URL . "../index.php");
   }
   // Seleção de Dados
 } else {
