85dee684add1dcbf138b3c5f7c083e2869a4dc8b
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-03-09 13:55:25 -0300

    add validations for data in process

--- config/process.php
+++ config/process.php
@@ -5,25 +5,37 @@ session_start();
 include_once("conn.php");
 include_once("url.php");
 
-$id;
-if (!empty($_GET)) {
-  $id = $_GET["id"];
-}
-
-// Retorna o dado de um contato
-if (!empty($id)) {
-  $query =  "SELECT * FROM contacts WHERE id = :id";
-
-  $stmt = $conn->prepare($query);
-  $stmt->bindParam(":id", $id);
-  $stmt->execute();
-  $contact = $stmt->fetch();
+$data = $_POST;
+// Modificações no Banco
+if (!empty($data)) {
+  print_r($data);
+  if ($data["type"] === "create") {
+  }
+  // Seleção de Dados
 } else {
-  // Retorna os dados de todos os contatos
-  $contacts = [];
+  $id;
 
-  $query = "SELECT * FROM contacts";
-  $stmt = $conn->prepare($query);
-  $stmt->execute();
-  $contacts = $stmt->fetchAll();
+  if (!empty($_GET)) {
+    $id = $_GET["id"];
+  }
+
+  // Retorna o dado de um contato
+  if (!empty($id)) {
+    $query =  "SELECT * FROM contacts WHERE id = :id";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":id", $id);
+    $stmt->execute();
+    $contact = $stmt->fetch();
+  } else {
+    // Retorna os dados de todos os contatos
+    $contacts = [];
+    $query = "SELECT * FROM contacts";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $contacts = $stmt->fetchAll();
+  }
 }
+
+
+// Fechar conexão
+$conn = null;
